7fd518a3b443a39fc8e90e40f0e1f3652c6726e9
michael-carvajal <michaelcarvajal.work@gmail.com>
2023-08-10 16:12:01 -0400

    used query selector to add data as content in schedule

---
+++ index.js
@@ -0,0 +1,63 @@
+window.onload = e => {
+    const mountain1 = {
+        1: {
+            date: "11 Jan 2024",
+            trail: "Sunset Ridge Trail"
+        },
+        2: {
+            date: "05 Apr 2024",
+            trail: "Forest Creek Trail"
+        },
+        3: {
+            date: "22 Aug 2024",
+            trail: "River View Trail"
+        },
+        4: {
+            date: "16 Nov 2024",
+            trail: "Mountain Peak Trail"
+        }
+    };
+
+    const mountain2 = {
+        1: {
+            date: "14 Mar 2024",
+            trail: "Canyon Explorer Trail"
+        },
+        2: {
+            date: "07 Jul 2024",
+            trail: "Lakeside Trail"
+        },
+        3: {
+            date: "28 Sep 2024",
+            trail: "Wildflower Path"
+        },
+        4: {
+            date: "02 Dec 2024",
+            trail: "Ridge Run Trail"
+        }
+    };
+
+    console.log(mountain1);
+    console.log(mountain2);
+
+    const events = document.querySelector('.events');
+    console.log(events);
+    for (let key in mountain1) {
+        const { date, trail } = mountain1[key]
+        console.log(date, trail);
+        const newTrailContainer = document.createElement('div')
+        const dateEle = document.createElement('p')
+        dateEle.innerText = date
+        const trailEle = document.createElement('p')
+        trailEle.innerText = trail
+        newTrailContainer.append(dateEle, trailEle)
+        newTrailContainer.className = 'ind-event'
+        events.appendChild(newTrailContainer)
+
+    }
+
+    console.log('hello world')
+
+
+
+}
